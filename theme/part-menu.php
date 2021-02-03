de939b902ed9eaddd452a4288712f0d8d94f8f4d
Maria Bartoszuk <mariabartoszuk@gmail.com>
2021-02-02 22:34:10 -0600

    add untracked files

---
+++ theme/part-menu.php
@@ -0,0 +1,39 @@
+<a class="wmk2020-nav-burger" href="#expand-menu">
+  <div class="wmk2020-burger-bar1"></div>
+  <div class="wmk2020-burger-bar2"></div>
+  <div class="wmk2020-burger-bar3"></div>
+</a>
+<div id="close-menu"></div>
+<section id="expand-menu">
+  <nav class="wmk2020-navbar">
+    <a class="wmk2020-nav-close" href="#close-menu">
+      <div class="wmk2020-nav-close-bar1"></div>
+      <div class="wmk2020-nav-close-bar2"></div>
+    </a>
+    <ul id="menu-nav" class="menu">
+      <li class="menu-item">
+        <a href="http://wmkchicago.org/" aria-current="page">
+          <span class="wmk2020-menu-item-text">Strona główna</span>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="http://wmkchicago.org/#-aktualnosci" aria-current="page">
+          <span class="wmk2020-menu-item-text">Aktualności</span>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="http://wmkchicago.org/#-wesprzyj-nas" aria-current="page">
+          <span class="wmk2020-menu-item-text">Wesprzyj nas</span>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="http://wmkchicago.org/#-kontakt" aria-current="page">
+          <span class="wmk2020-menu-item-text">Kontakt</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <a class="wmk2020-navbar-close" href="#close-menu">
+    <span class="wmk2020-navbar-close-text">Close menu</span>
+  </a>
+</section>
