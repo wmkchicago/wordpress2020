2868e7c9d2fc276ee6ccd4d37e6328d2891461f9
Maria Bartoszuk <mariabartoszuk@gmail.com>
2021-02-03 21:18:14 -0600

    menu items weight 700

--- theme/header.php
+++ theme/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="preconnect" href="https://fonts.gstatic.com"/>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&family=Eczar:wght@400;500;600&display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&family=Eczar:wght@400;500;600&display=swap" rel="stylesheet"/>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
