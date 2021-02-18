bdebc8ad4c3ef235f891f9e13fc84e6e4788d83d
Maria Bartoszuk <mariabartoszuk@gmail.com>
2021-02-17 21:11:33 -0600

    footer layout

--- theme/footer.php
+++ theme/footer.php
@@ -5,10 +5,14 @@
   </nav>
   <address class="wmk2020-footer-contact-info">
     <p>
-      Serdecznie dziękujemy za zainteresowanie naszą wspólnotą. Chętnie nawiążemy kontakt lub odpowiemy na pytania jakie możecie mieć o naszej wspólnocie, sposobie naszej działalności, lub nadchodzących i planowanych wydarzeniach.
+      Serdecznie dziękujemy za&nbsp;zainteresowanie naszą wspólnotą. Chętnie nawiążemy kontakt lub&nbsp;odpowiemy na&nbsp;pytania jakie możecie mieć o&nbsp;naszej wspólnocie, sposobie naszej działalności, lub&nbsp;nadchodzących i&nbsp;planowanych wydarzeniach.
     </p>
     <a href="mailto:kontakt@wmkchicago.org">kontakt@wmkchicago.org</a>
   </address>
+  <div class="wmk2020-footer-logo">
+    <img class="wmk2020-footer-logo-img" src="<?php echo wp_get_attachment_image_src(58, 'medium')[0]; ?>"/>
+    <p>&copy; 2021 All Rights Reserved</p>
+  </div>
 </footer>
 <? wp_footer(); ?>
 </body>
